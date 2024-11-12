f0f366e8cf38e26f01829d4dadc190a5a0129f67
Evins <MAEvins@cps.k12.il.us>
2024-11-12 13:16:21 -0600

    first commit

--- student_data.py
+++ student_data.py
@@ -178,4 +178,148 @@ students = [{
     "HR": "B230",
     "GL": 10,
     "Email": ["donnajones@example.com", "donna.j@example.net"]
-}] 
+}, {
+    "CPSID": 10000031,
+    "Combo,Name": "Knight, George",
+    "LName": "Knight",
+    "FName": "George",
+    "MName": "Henry",
+    "HR": "B231",
+    "GL": 10,
+    "Email": ["georgeknight@example.com", "g.knight@example.net"]
+}, {
+    "CPSID": 10000032,
+    "Combo,Name": "Lowe, Hannah",
+    "LName": "Lowe",
+    "FName": "Hannah",
+    "MName": "Isabel",
+    "HR": "B232",
+    "GL": 10,
+    "Email": ["hannahlowe@example.com", "h.lowe@example.net"]
+}, {
+    "CPSID": 10000033,
+    "Combo,Name": "Morris, Ian",
+    "LName": "Morris",
+    "FName": "Ian",
+    "MName": "James",
+    "HR": "B233",
+    "GL": 10,
+    "Email": ["ianmorris@example.com", "ian.m@example.net"]
+}, {
+    "CPSID": 10000034,
+    "Combo,Name": "Nash, Julia",
+    "LName": "Nash",
+    "FName": "Julia",
+    "MName": "Kate",
+    "HR": "B234",
+    "GL": 10,
+    "Email": ["julianash@example.com", "j.nash@example.net"]
+}, {
+    "CPSID": 10000035,
+    "Combo,Name": "Owens, Kevin",
+    "LName": "Owens",
+    "FName": "Kevin",
+    "MName": "Leon",
+    "HR": "B235",
+    "GL": 10,
+    "Email": ["kevinowens@example.com", "k.owens@example.net"]
+}, {
+    "CPSID": 10000036,
+    "Combo,Name": "Perry, Lisa",
+    "LName": "Perry",
+    "FName": "Lisa",
+    "MName": "Marie",
+    "HR": "B236",
+    "GL": 10,
+    "Email": ["lisaperry@example.com", "l.perry@example.net"]
+}, {
+    "CPSID": 10000037,
+    "Combo,Name": "Quinn, Mark",
+    "LName": "Quinn",
+    "FName": "Mark",
+    "MName": "Nathan",
+    "HR": "B237",
+    "GL": 10,
+    "Email": ["markquinn@example.com", "m.quinn@example.net"]
+}, {
+    "CPSID": 10000038,
+    "Combo,Name": "Reed, Olivia",
+    "LName": "Reed",
+    "FName": "Olivia",
+    "MName": "Paige",
+    "HR": "B238",
+    "GL": 10,
+    "Email": ["oliviareed@example.com", "olivia.r@example.net"]
+}, {
+    "CPSID": 10000039,
+    "Combo,Name": "Scott, Peter",
+    "LName": "Scott",
+    "FName": "Peter",
+    "MName": "Quentin",
+    "HR": "B239",
+    "GL": 10,
+    "Email": ["peterscott@example.com", "p.scott@example.net"]
+}, {
+    "CPSID": 10000040,
+    "Combo,Name": "Turner, Quinn",
+    "LName": "Turner",
+    "FName": "Quinn",
+    "MName": "Rachel",
+    "HR": "B240",
+    "GL": 10,
+    "Email": ["quinnt@example.com", "q.turner@example.net"]
+}, {
+    "CPSID": 10000041,
+    "Combo,Name": "Upton, Sam",
+    "LName": "Upton",
+    "FName": "Sam",
+    "MName": "Thomas",
+    "HR": "B241",
+    "GL": 10,
+    "Email": ["samupton@example.com", "s.upton@example.net"]
+}, {
+    "CPSID": 10000042,
+    "Combo,Name": "Vega, Tina",
+    "LName": "Vega",
+    "FName": "Tina",
+    "MName": "Uma",
+    "HR": "B242",
+    "GL": 10,
+    "Email": ["tinavega@example.com", "t.vega@example.net"]
+}, {
+    "CPSID": 10000043,
+    "Combo,Name": "Watson, Uri",
+    "LName": "Watson",
+    "FName": "Uri",
+    "MName": "Victor",
+    "HR": "B243",
+    "GL": 10,
+    "Email": ["uriwatson@example.com", "u.watson@example.net"]
+}, {
+    "CPSID": 10000044,
+    "Combo,Name": "Xavier, Vicky",
+    "LName": "Xavier",
+    "FName": "Vicky",
+    "MName": "Willow",
+    "HR": "B244",
+    "GL": 10,
+    "Email": ["vickyxavier@example.com", "v.xavier@example.net"]
+}, {
+    "CPSID": 10000045,
+    "Combo,Name": "Young, Will",
+    "LName": "Young",
+    "FName": "Will",
+    "MName": "Xander",
+    "HR": "B245",
+    "GL": 10,
+    "Email": ["willyoung@example.com", "will.y@example.net"]
+}, {
+    "CPSID": 10000046,
+    "Combo,Name": "Zane, Zoe",
+    "LName": "Zane",
+    "FName": "Zoe",
+    "MName": "Yvonne",
+    "HR": "B246",
+    "GL": 10,
+    "Email": ["zoezane@example.com", "z.zane@example.net"]
+}]
