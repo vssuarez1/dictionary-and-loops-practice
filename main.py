4cef87c7d5a1677c7120713ad44715bb1682b6f1
Suarez <vssuarez1@cps.k12.il.us>
2024-11-20 08:55:20 -0600

    data

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import student_data
 
 # print(student_data.students)
 students = student_data.students
-print(len(students))
-print(students[0]['Combo,Name'])
-print(students[0]['Email'][0])
-print(students[0]['Email'][1])
+# print(len(students))
+# print(students[0]['Combo,Name'])
+# print(students[0]['Email'][0])
+# print(students[0]['Email'][1])
+# print(students[0]['HR'])
+# print(students[0]['GL'])
+# print(students[0]['CPSID'])
 
 # for loops allow us to
 #iterate through the data
@@ -16,23 +19,219 @@ print(students[0]['Email'][1])
 #and printing the name and email of the students
 #we are also printing a line of underscores to separate the students
 #we are also printing a line of underscores to separate the students
-for student in students:
-    print(student['Combo,Name'])
-    print(student['Email'][0])
-    print(student['Email'][1])
-    print("_"*25)
+# for student in students:
+#     print(student['Combo,Name'])
+#     print(student['Email'][0])
+#     print(student['Email'][1])
+#     print(students['HR'])
+#     print("_"*25)
+
 
 
 # we are asking the user to input their name
 # then we are checking if the name is in the data
 # if the name is in the data we are printing the name and "this works"
-name = input("what is you name?") 
-for student in students:
-    if name == student['Combo,Name']:
-        print(student['Combo,Name'])
-        print("this works")
+# name = input("what is your name?") 
+# id = int(input("What is your id?"))
+# for student in students:
+#     if id == student['CPSID']:
+#         print(student['Combo,Name'])
+#         print("this works")
 
 
 # Let's try to print out the emails of the students:    
+
+
+
+# 1. PRINT STUDENT DETAILS
+# for student in students:
+#     print("Name: " + student['FName'] + " " + student['LName'])
+#     print("Email 1: " + student['Email'][0])
+#     print("Email 2: " + student['Email'][1])
+#     print("_"*25)
+
+# # 2. SEARCH FOR STUDENTS IN A SPECIFIC HOMEROOM 
+# HR_code = input("Enter a homeroom code: ") 
+# for student in students:
+#     if HR_code == student['HR']:
+#         print(student['Combo,Name'])
+#         print(student['Email'][0])
+#         break
+# else:
+#         print("No students found in this homeroom.")
+
+
+
+
+# # 3.CHECK IF A STUDENT IS IN A LIST
+# name=input("What is your first name?")
+# for student in students:
+#     print(student['Combo,Name'])
+#     print(student['HR'])
+
+
+
+# # 4. FILTER STUDENTS BY GRADE LEVEL
+# for student in students:
+#     if student =="GL":10
+#     print(student['Combo,Name'])
+#     print(student['CPSID'])
+#     print("_"*25)
+
+
+# # # 5. FORMAT EMAIL LIST FOR NEWSLETTER
+# for student in students:
+#     print(student['Email'][0] + "      " + student['Email'][1])
+
+
+
+
+# Update the dataset in memory (e.g., modify student details)
+for student in students:
+    if student['CPSID'] == 10000014:  # Example CPSID to update
+        student['FName'] = 'Noah'
+        student['LName'] = 'Hernandez'
+        student['ContactNumber'] = '987-654-3210' 
+
+for student in students:
+    if student['CPSID'] == 10000021:  # Replace with the condition to find the specific student
+        # del student['MName'] # Deletes the 'MName' key-value pair
+        # del student['GL'] 
+        # del student['HR']
+        # del student['Email'][0]
+        print(f"Updated student: {student}")
+
+# Update a specific entry by index
+# students[4]['FName'] = 'Ruby'  # Updates the first student's first name
+# students[4]['LName'] = 'Solis'
+# print(students[4])
+
+# Remove a specific student by index
+del students[-1]  # Removes the first student in the list
+
+# Example: Add a 'ContactNumber' field to each student
+# for student in students:
+#     student['ContactNumber'] = '123-456-7890'  # Assign a default or specific value
+
+# Create a new student dictionary
+new_student = {
+    'CPSID': 987654,
+    'Combo,Name': 'Doe, John',
+    'FName': 'John',
+    'LName': 'Doe',
+    'MName': 'Paul',
+    'HR': 'B220',
+    'GL': 11,
+    'Email': ['john.doe@example.com', 'j.doe@example.org']
+}
+
+# Add the new student to the list
+# students.append(new_student)
+
+# Collecting input from the user for each field
+cpsid = int(input("Enter CPSID: "))
+combo_name = input("Enter Combo,Name (Last, First): ")
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+middle_name = input("Enter Middle Name: ")
+homeroom = input("Enter Homeroom (e.g., B220): ")
+grade_level = int(input("Enter Grade Level: "))
+primary_email = input("Enter Primary Email: ")
+secondary_email = input("Enter Secondary Email: ")
+
+# Create a new student dictionary using the input
+new_student = {
+    'CPSID': cpsid,
+    'Combo,Name': combo_name,
+    'FName': first_name,
+    'LName': last_name,
+    'MName': middle_name,
+    'HR': homeroom,
+    'GL': grade_level,
+    'Email': [primary_email, secondary_email]
+}
+
+# Add the new student to the list
+students.append(new_student)
+
+# Print confirmation and the new student details
+print("New student added:")
+print(new_student)
+
+# Example: Update multiple fields for students in a specific homeroom
+for student in students:
+    if student['HR'] == 'B211':  # Condition to find specific students
+        student.update({
+            'FName': 'UpdatedFirstName',
+            'LName': 'UpdatedLastName',
+            'Email': ['new.email@example.com', 'secondary.email@example.com']
+        })
+        print(f"Updated student: {student}")
+
+# Update a student's information directly by index
+students[0]['FName'] = 'Michael'
+students[0]['LName'] = 'Smith'
+students[0]['Email'] = ['michael.smith@example.com']
+print(students[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Overwrite the `student_data.py` file with the updated data
+# Overwrite the student_data.py file with formatted data
+with open('student_data.py', 'w') as f:
+    f.write("students = [\n")
+    for student in students:
+        formatted_student = "    {\n"
+        for key, value in student.items():
+            formatted_student += f"        '{key}': {repr(value)},\n"
+        formatted_student = formatted_student.rstrip(",\n") + "\n    },\n"  # Clean trailing commas and newline
+        f.write(formatted_student)
+    f.write("]\n")
+
+print("student_data.py has been updated with the original formatting.")
+
+
+
+
+
+
+
 
 
--- student_data.py
+++ student_data.py
@@ -1,325 +1,383 @@
-students = [{
-    "CPSID": 10000011,
-    "Combo,Name": "Anderson, Karen",
-    "LName": "Anderson",
-    "FName": "Karen",
-    "MName": "Lynn",
-    "HR": "B211",
-    "GL": 10,
-    "Email": ["karenanderson@example.com", "karen.a@example.net"]
-}, {
-    "CPSID": 10000012,
-    "Combo,Name": "Thomas, Liam",
-    "LName": "Thomas",
-    "FName": "Liam",
-    "MName": "Oliver",
-    "HR": "B212",
-    "GL": 10,
-    "Email": ["liamthomas@example.com", "l.thomas@example.net"]
-}, {
-    "CPSID": 10000013,
-    "Combo,Name": "Martinez, Mia",
-    "LName": "Martinez",
-    "FName": "Mia",
-    "MName": "Nicole",
-    "HR": "B213",
-    "GL": 10,
-    "Email": ["miamartinez@example.com", "mia.mtz@example.net"]
-}, {
-    "CPSID": 10000014,
-    "Combo,Name": "Hernandez, Noah",
-    "LName": "Hernandez",
-    "FName": "Noah",
-    "MName": "Peter",
-    "HR": "B214",
-    "GL": 10,
-    "Email": ["noahhernandez@example.com", "noah.hdz@example.net"]
-}, {
-    "CPSID": 10000015,
-    "Combo,Name": "Lopez, Olivia",
-    "LName": "Lopez",
-    "FName": "Olivia",
-    "MName": "Quinn",
-    "HR": "B215",
-    "GL": 10,
-    "Email": ["olivialopez@example.com", "olivia.lop@example.net"]
-}, {
-    "CPSID": 10000016,
-    "Combo,Name": "King, Paul",
-    "LName": "King",
-    "FName": "Paul",
-    "MName": "Robert",
-    "HR": "B216",
-    "GL": 10,
-    "Email": ["paulking@example.com", "p.king@example.net"]
-}, {
-    "CPSID": 10000017,
-    "Combo,Name": "Clark, Quinn",
-    "LName": "Clark",
-    "FName": "Quinn",
-    "MName": "Stella",
-    "HR": "B217",
-    "GL": 10,
-    "Email": ["quinnclark@example.com", "quinn.cl@example.net"]
-}, {
-    "CPSID": 10000018,
-    "Combo,Name": "Young, Rachel",
-    "LName": "Young",
-    "FName": "Rachel",
-    "MName": "Tara",
-    "HR": "B218",
-    "GL": 10,
-    "Email": ["rachelyoung@example.com", "r.young@example.net"]
-}, {
-    "CPSID": 10000019,
-    "Combo,Name": "Walker, Steve",
-    "LName": "Walker",
-    "FName": "Steve",
-    "MName": "Ulysses",
-    "HR": "B219",
-    "GL": 10,
-    "Email": ["stevewalker@example.com", "steve.walk@example.net"]
-}, {
-    "CPSID": 10000020,
-    "Combo,Name": "White, Tina",
-    "LName": "White",
-    "FName": "Tina",
-    "MName": "Violet",
-    "HR": "B220",
-    "GL": 10,
-    "Email": ["tinawhite@example.com", "tina.w@example.net"]
-}, {
-    "CPSID": 10000021,
-    "Combo,Name": "Adams, Uriel",
-    "LName": "Adams",
-    "FName": "Uriel",
-    "MName": "William",
-    "HR": "B221",
-    "GL": 10,
-    "Email": ["urieladams@example.com", "uriel.ad@example.net"]
-}, {
-    "CPSID": 10000022,
-    "Combo,Name": "Bennett, Victoria",
-    "LName": "Bennett",
-    "FName": "Victoria",
-    "MName": "Xena",
-    "HR": "B222",
-    "GL": 10,
-    "Email": ["victoriabennett@example.com", "vicky.b@example.net"]
-}, {
-    "CPSID": 10000023,
-    "Combo,Name": "Carter, Wyatt",
-    "LName": "Carter",
-    "FName": "Wyatt",
-    "MName": "Yale",
-    "HR": "B223",
-    "GL": 10,
-    "Email": ["wyattcarter@example.com", "wyatt.c@example.net"]
-}, {
-    "CPSID": 10000024,
-    "Combo,Name": "Dixon, Ximena",
-    "LName": "Dixon",
-    "FName": "Ximena",
-    "MName": "Zara",
-    "HR": "B224",
-    "GL": 10,
-    "Email": ["ximenadixon@example.com", "ximena.d@example.net"]
-}, {
-    "CPSID": 10000025,
-    "Combo,Name": "Evans, Yosef",
-    "LName": "Evans",
-    "FName": "Yosef",
-    "MName": "Aaron",
-    "HR": "B225",
-    "GL": 10,
-    "Email": ["yosefevans@example.com", "yosef.e@example.net"]
-}, {
-    "CPSID": 10000026,
-    "Combo,Name": "Flores, Zoe",
-    "LName": "Flores",
-    "FName": "Zoe",
-    "MName": "Brian",
-    "HR": "B226",
-    "GL": 10,
-    "Email": ["zoeflores@example.com", "zoe.f@example.net"]
-}, {
-    "CPSID": 10000027,
-    "Combo,Name": "Green, Alan",
-    "LName": "Green",
-    "FName": "Alan",
-    "MName": "Charlie",
-    "HR": "B227",
-    "GL": 10,
-    "Email": ["alangreen@example.com", "alan.g@example.net"]
-}, {
-    "CPSID": 10000028,
-    "Combo,Name": "Hill, Bella",
-    "LName": "Hill",
-    "FName": "Bella",
-    "MName": "Diane",
-    "HR": "B228",
-    "GL": 10,
-    "Email": ["bellahill@example.com", "bella.h@example.net"]
-}, {
-    "CPSID": 10000029,
-    "Combo,Name": "Irwin, Carl",
-    "LName": "Irwin",
-    "FName": "Carl",
-    "MName": "Edward",
-    "HR": "B229",
-    "GL": 10,
-    "Email": ["carlirwin@example.com", "carl.i@example.net"]
-}, {
-    "CPSID": 10000030,
-    "Combo,Name": "Jones, Donna",
-    "LName": "Jones",
-    "FName": "Donna",
-    "MName": "Francine",
-    "HR": "B230",
-    "GL": 10,
-    "Email": ["donnajones@example.com", "donna.j@example.net"]
-}, {
-    "CPSID": 10000031,
-    "Combo,Name": "Knight, George",
-    "LName": "Knight",
-    "FName": "George",
-    "MName": "Henry",
-    "HR": "B231",
-    "GL": 10,
-    "Email": ["georgeknight@example.com", "g.knight@example.net"]
-}, {
-    "CPSID": 10000032,
-    "Combo,Name": "Lowe, Hannah",
-    "LName": "Lowe",
-    "FName": "Hannah",
-    "MName": "Isabel",
-    "HR": "B232",
-    "GL": 10,
-    "Email": ["hannahlowe@example.com", "h.lowe@example.net"]
-}, {
-    "CPSID": 10000033,
-    "Combo,Name": "Morris, Ian",
-    "LName": "Morris",
-    "FName": "Ian",
-    "MName": "James",
-    "HR": "B233",
-    "GL": 10,
-    "Email": ["ianmorris@example.com", "ian.m@example.net"]
-}, {
-    "CPSID": 10000034,
-    "Combo,Name": "Nash, Julia",
-    "LName": "Nash",
-    "FName": "Julia",
-    "MName": "Kate",
-    "HR": "B234",
-    "GL": 10,
-    "Email": ["julianash@example.com", "j.nash@example.net"]
-}, {
-    "CPSID": 10000035,
-    "Combo,Name": "Owens, Kevin",
-    "LName": "Owens",
-    "FName": "Kevin",
-    "MName": "Leon",
-    "HR": "B235",
-    "GL": 10,
-    "Email": ["kevinowens@example.com", "k.owens@example.net"]
-}, {
-    "CPSID": 10000036,
-    "Combo,Name": "Perry, Lisa",
-    "LName": "Perry",
-    "FName": "Lisa",
-    "MName": "Marie",
-    "HR": "B236",
-    "GL": 10,
-    "Email": ["lisaperry@example.com", "l.perry@example.net"]
-}, {
-    "CPSID": 10000037,
-    "Combo,Name": "Quinn, Mark",
-    "LName": "Quinn",
-    "FName": "Mark",
-    "MName": "Nathan",
-    "HR": "B237",
-    "GL": 10,
-    "Email": ["markquinn@example.com", "m.quinn@example.net"]
-}, {
-    "CPSID": 10000038,
-    "Combo,Name": "Reed, Olivia",
-    "LName": "Reed",
-    "FName": "Olivia",
-    "MName": "Paige",
-    "HR": "B238",
-    "GL": 10,
-    "Email": ["oliviareed@example.com", "olivia.r@example.net"]
-}, {
-    "CPSID": 10000039,
-    "Combo,Name": "Scott, Peter",
-    "LName": "Scott",
-    "FName": "Peter",
-    "MName": "Quentin",
-    "HR": "B239",
-    "GL": 10,
-    "Email": ["peterscott@example.com", "p.scott@example.net"]
-}, {
-    "CPSID": 10000040,
-    "Combo,Name": "Turner, Quinn",
-    "LName": "Turner",
-    "FName": "Quinn",
-    "MName": "Rachel",
-    "HR": "B240",
-    "GL": 10,
-    "Email": ["quinnt@example.com", "q.turner@example.net"]
-}, {
-    "CPSID": 10000041,
-    "Combo,Name": "Upton, Sam",
-    "LName": "Upton",
-    "FName": "Sam",
-    "MName": "Thomas",
-    "HR": "B241",
-    "GL": 10,
-    "Email": ["samupton@example.com", "s.upton@example.net"]
-}, {
-    "CPSID": 10000042,
-    "Combo,Name": "Vega, Tina",
-    "LName": "Vega",
-    "FName": "Tina",
-    "MName": "Uma",
-    "HR": "B242",
-    "GL": 10,
-    "Email": ["tinavega@example.com", "t.vega@example.net"]
-}, {
-    "CPSID": 10000043,
-    "Combo,Name": "Watson, Uri",
-    "LName": "Watson",
-    "FName": "Uri",
-    "MName": "Victor",
-    "HR": "B243",
-    "GL": 10,
-    "Email": ["uriwatson@example.com", "u.watson@example.net"]
-}, {
-    "CPSID": 10000044,
-    "Combo,Name": "Xavier, Vicky",
-    "LName": "Xavier",
-    "FName": "Vicky",
-    "MName": "Willow",
-    "HR": "B244",
-    "GL": 10,
-    "Email": ["vickyxavier@example.com", "v.xavier@example.net"]
-}, {
-    "CPSID": 10000045,
-    "Combo,Name": "Young, Will",
-    "LName": "Young",
-    "FName": "Will",
-    "MName": "Xander",
-    "HR": "B245",
-    "GL": 10,
-    "Email": ["willyoung@example.com", "will.y@example.net"]
-}, {
-    "CPSID": 10000046,
-    "Combo,Name": "Zane, Zoe",
-    "LName": "Zane",
-    "FName": "Zoe",
-    "MName": "Yvonne",
-    "HR": "B246",
-    "GL": 10,
-    "Email": ["zoezane@example.com", "z.zane@example.net"]
-}]
+students = [
+    {
+        'CPSID': 10000012,
+        'Combo,Name': 'Thomas, Liam',
+        'LName': 'Thomas',
+        'FName': 'Liam',
+        'MName': 'Oliver',
+        'HR': 'B212',
+        'GL': 10,
+        'Email': ['liamthomas@example.com', 'l.thomas@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000013,
+        'Combo,Name': 'Martinez, Mia',
+        'LName': 'Martinez',
+        'FName': 'Mia',
+        'MName': 'Nicole',
+        'HR': 'B213',
+        'GL': 10,
+        'Email': ['miamartinez@example.com', 'mia.mtz@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000014,
+        'Combo,Name': 'Hernandez, Noah',
+        'LName': 'Hernandez',
+        'FName': 'Noah',
+        'MName': 'Peter',
+        'HR': 'B214',
+        'GL': 10,
+        'Email': ['noahhernandez@example.com', 'noah.hdz@example.net'],
+        'ContactNumber': '987-654-3210'
+    },
+    {
+        'CPSID': 10000015,
+        'Combo,Name': 'Lopez, Olivia',
+        'LName': 'Solis',
+        'FName': 'Ruby',
+        'MName': 'Quinn',
+        'HR': 'B215',
+        'GL': 10,
+        'Email': ['olivialopez@example.com', 'olivia.lop@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000016,
+        'Combo,Name': 'King, Paul',
+        'LName': 'King',
+        'FName': 'Paul',
+        'MName': 'Robert',
+        'HR': 'B216',
+        'GL': 10,
+        'Email': ['paulking@example.com', 'p.king@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000017,
+        'Combo,Name': 'Clark, Quinn',
+        'LName': 'Clark',
+        'FName': 'Quinn',
+        'MName': 'Stella',
+        'HR': 'B217',
+        'GL': 10,
+        'Email': ['quinnclark@example.com', 'quinn.cl@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000018,
+        'Combo,Name': 'Young, Rachel',
+        'LName': 'Young',
+        'FName': 'Rachel',
+        'MName': 'Tara',
+        'HR': 'B218',
+        'GL': 10,
+        'Email': ['rachelyoung@example.com', 'r.young@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000019,
+        'Combo,Name': 'Walker, Steve',
+        'LName': 'Walker',
+        'FName': 'Steve',
+        'MName': 'Ulysses',
+        'HR': 'B219',
+        'GL': 10,
+        'Email': ['stevewalker@example.com', 'steve.walk@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000020,
+        'Combo,Name': 'White, Tina',
+        'LName': 'White',
+        'FName': 'Tina',
+        'MName': 'Violet',
+        'HR': 'B220',
+        'GL': 10,
+        'Email': ['tinawhite@example.com', 'tina.w@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000021,
+        'Combo,Name': 'Adams, Uriel',
+        'LName': 'Adams',
+        'FName': 'Uriel',
+        'Email': ['uriel.ad@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000022,
+        'Combo,Name': 'Bennett, Victoria',
+        'LName': 'Bennett',
+        'FName': 'Victoria',
+        'MName': 'Xena',
+        'HR': 'B222',
+        'GL': 10,
+        'Email': ['victoriabennett@example.com', 'vicky.b@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000023,
+        'Combo,Name': 'Carter, Wyatt',
+        'LName': 'Carter',
+        'FName': 'Wyatt',
+        'MName': 'Yale',
+        'HR': 'B223',
+        'GL': 10,
+        'Email': ['wyattcarter@example.com', 'wyatt.c@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000024,
+        'Combo,Name': 'Dixon, Ximena',
+        'LName': 'Dixon',
+        'FName': 'Ximena',
+        'MName': 'Zara',
+        'HR': 'B224',
+        'GL': 10,
+        'Email': ['ximenadixon@example.com', 'ximena.d@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000025,
+        'Combo,Name': 'Evans, Yosef',
+        'LName': 'Evans',
+        'FName': 'Yosef',
+        'MName': 'Aaron',
+        'HR': 'B225',
+        'GL': 10,
+        'Email': ['yosefevans@example.com', 'yosef.e@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000026,
+        'Combo,Name': 'Flores, Zoe',
+        'LName': 'Flores',
+        'FName': 'Zoe',
+        'MName': 'Brian',
+        'HR': 'B226',
+        'GL': 10,
+        'Email': ['zoeflores@example.com', 'zoe.f@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000027,
+        'Combo,Name': 'Green, Alan',
+        'LName': 'Green',
+        'FName': 'Alan',
+        'MName': 'Charlie',
+        'HR': 'B227',
+        'GL': 10,
+        'Email': ['alangreen@example.com', 'alan.g@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000028,
+        'Combo,Name': 'Hill, Bella',
+        'LName': 'Hill',
+        'FName': 'Bella',
+        'MName': 'Diane',
+        'HR': 'B228',
+        'GL': 10,
+        'Email': ['bellahill@example.com', 'bella.h@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000029,
+        'Combo,Name': 'Irwin, Carl',
+        'LName': 'Irwin',
+        'FName': 'Carl',
+        'MName': 'Edward',
+        'HR': 'B229',
+        'GL': 10,
+        'Email': ['carlirwin@example.com', 'carl.i@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000030,
+        'Combo,Name': 'Jones, Donna',
+        'LName': 'Jones',
+        'FName': 'Donna',
+        'MName': 'Francine',
+        'HR': 'B230',
+        'GL': 10,
+        'Email': ['donnajones@example.com', 'donna.j@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000031,
+        'Combo,Name': 'Knight, George',
+        'LName': 'Knight',
+        'FName': 'George',
+        'MName': 'Henry',
+        'HR': 'B231',
+        'GL': 10,
+        'Email': ['georgeknight@example.com', 'g.knight@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000032,
+        'Combo,Name': 'Lowe, Hannah',
+        'LName': 'Lowe',
+        'FName': 'Hannah',
+        'MName': 'Isabel',
+        'HR': 'B232',
+        'GL': 10,
+        'Email': ['hannahlowe@example.com', 'h.lowe@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000033,
+        'Combo,Name': 'Morris, Ian',
+        'LName': 'Morris',
+        'FName': 'Ian',
+        'MName': 'James',
+        'HR': 'B233',
+        'GL': 10,
+        'Email': ['ianmorris@example.com', 'ian.m@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000034,
+        'Combo,Name': 'Nash, Julia',
+        'LName': 'Nash',
+        'FName': 'Julia',
+        'MName': 'Kate',
+        'HR': 'B234',
+        'GL': 10,
+        'Email': ['julianash@example.com', 'j.nash@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000035,
+        'Combo,Name': 'Owens, Kevin',
+        'LName': 'Owens',
+        'FName': 'Kevin',
+        'MName': 'Leon',
+        'HR': 'B235',
+        'GL': 10,
+        'Email': ['kevinowens@example.com', 'k.owens@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000036,
+        'Combo,Name': 'Perry, Lisa',
+        'LName': 'Perry',
+        'FName': 'Lisa',
+        'MName': 'Marie',
+        'HR': 'B236',
+        'GL': 10,
+        'Email': ['lisaperry@example.com', 'l.perry@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000037,
+        'Combo,Name': 'Quinn, Mark',
+        'LName': 'Quinn',
+        'FName': 'Mark',
+        'MName': 'Nathan',
+        'HR': 'B237',
+        'GL': 10,
+        'Email': ['markquinn@example.com', 'm.quinn@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000038,
+        'Combo,Name': 'Reed, Olivia',
+        'LName': 'Reed',
+        'FName': 'Olivia',
+        'MName': 'Paige',
+        'HR': 'B238',
+        'GL': 10,
+        'Email': ['oliviareed@example.com', 'olivia.r@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000039,
+        'Combo,Name': 'Scott, Peter',
+        'LName': 'Scott',
+        'FName': 'Peter',
+        'MName': 'Quentin',
+        'HR': 'B239',
+        'GL': 10,
+        'Email': ['peterscott@example.com', 'p.scott@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000040,
+        'Combo,Name': 'Turner, Quinn',
+        'LName': 'Turner',
+        'FName': 'Quinn',
+        'MName': 'Rachel',
+        'HR': 'B240',
+        'GL': 10,
+        'Email': ['quinnt@example.com', 'q.turner@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000041,
+        'Combo,Name': 'Upton, Sam',
+        'LName': 'Upton',
+        'FName': 'Sam',
+        'MName': 'Thomas',
+        'HR': 'B241',
+        'GL': 10,
+        'Email': ['samupton@example.com', 's.upton@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000042,
+        'Combo,Name': 'Vega, Tina',
+        'LName': 'Vega',
+        'FName': 'Tina',
+        'MName': 'Uma',
+        'HR': 'B242',
+        'GL': 10,
+        'Email': ['tinavega@example.com', 't.vega@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000043,
+        'Combo,Name': 'Watson, Uri',
+        'LName': 'Watson',
+        'FName': 'Uri',
+        'MName': 'Victor',
+        'HR': 'B243',
+        'GL': 10,
+        'Email': ['uriwatson@example.com', 'u.watson@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000044,
+        'Combo,Name': 'Xavier, Vicky',
+        'LName': 'Xavier',
+        'FName': 'Vicky',
+        'MName': 'Willow',
+        'HR': 'B244',
+        'GL': 10,
+        'Email': ['vickyxavier@example.com', 'v.xavier@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 10000045,
+        'Combo,Name': 'Young, Will',
+        'LName': 'Young',
+        'FName': 'Will',
+        'MName': 'Xander',
+        'HR': 'B245',
+        'GL': 10,
+        'Email': ['willyoung@example.com', 'will.y@example.net'],
+        'ContactNumber': '123-456-7890'
+    },
+    {
+        'CPSID': 64554445657,
+        'Combo,Name': 'Solis, Vanessa',
+        'FName': 'Vanessa',
+        'LName': 'Solis',
+        'MName': 'Anabel',
+        'HR': '601',
+        'GL': 11,
+        'Email': ['vssuarez1', 'rsolis']
+    },
+]
